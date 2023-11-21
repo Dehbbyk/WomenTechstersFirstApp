824a482616c754f2231c70dcd127a8b8b44c50e8
HP <deborah.oyinda@yahoo.com>
2023-11-21 18:50:02 +0100

    Login page

--- app/src/main/java/com/debbie/womentechsters/MainActivity.kt
+++ app/src/main/java/com/debbie/womentechsters/MainActivity.kt
@@ -2,10 +2,19 @@ package com.debbie.womentechsters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import javax.security.auth.login.LoginException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val joinButton:Button = this.findViewById(R.id.joinNowButton)
+
+        joinButton.setOnClickListener{
+
+        }
     }
+
 }
