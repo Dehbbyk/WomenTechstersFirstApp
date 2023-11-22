88bc27d73c5bd48034e9cfdb9842533cf0b62d34
HP <deborah.oyinda@yahoo.com>
2023-11-22 19:13:06 +0100

    Create new account page

---
+++ app/src/main/java/com/debbie/womentechsters/CreateNewAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.debbie.womentechsters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class CreateNewAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_new_account)
+    }
+}
--- app/src/main/java/com/debbie/womentechsters/LoginActivity.kt
+++ app/src/main/java/com/debbie/womentechsters/LoginActivity.kt
@@ -1,11 +1,19 @@
 package com.debbie.womentechsters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val createNAButton: Button = findViewById(R.id.createNewAccount)
+        createNAButton.setOnClickListener{
+            val intent = Intent (this, CreateNewAccountActivity:: class.java)
+            startActivity(intent)
+        }
+
     }
 }
